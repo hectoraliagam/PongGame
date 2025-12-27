7a9816f16c14299837b05da11ac1d238299061cb
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-27 09:58:46 -0500

    redo main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include <raylib.h>
 #include "config.h"
+#include "ball.h"
+#include "PlayerPaddle.h"
+#include "CpuPaddle.h"
+
+Ball ball;
+PlayerPaddle player;
+CpuPaddle cpu;
 
 int main()
 {
-    InitWindow(Config::screenWidth, Config::screenHeight, "Pong Game");
-    SetTargetFPS(Config::FPS);
+  InitWindow(Config::screenWidth, Config::screenHeight, "Pong Game");
+  SetTargetFPS(Config::FPS);
 
-    while (WindowShouldClose() == false)
-    {
-        BeginDrawing();
-        ClearBackground(Config::darkBlue);
+  while (WindowShouldClose() == false)
+  {
+    // ===== Updating =====
+    ball.Update();
+    player.Update();
+    cpu.Update(ball.GetCenterY());
 
-        DrawLine(Config::lineStartPosX, Config::lineStartPosY, Config::lineEndPosX, Config::lineEndPosY, WHITE);
-        DrawCircle(Config::ballCenterX, Config::ballCenterY, Config::ballRadius, WHITE);
-        DrawRectangle(Config::paddleLeftPosX, Config::paddlePosY, Config::paddleWidth, Config::paddleHeight, WHITE);
-        DrawRectangle(Config::paddleRightPosX, Config::paddlePosY, Config::paddleWidth, Config::paddleHeight, WHITE);
+    // ===== Drawing =====
+    BeginDrawing();
+    ClearBackground(Config::darkBlue);
 
-        EndDrawing();
-    }
+    DrawLine(Config::lineStartPosX, Config::lineStartPosY, Config::lineEndPosX, Config::lineEndPosY, WHITE);
+    ball.Draw();
+    player.Draw();
+    cpu.Draw();
 
-    CloseWindow();
+    EndDrawing();
+  }
+
+  CloseWindow();
 }
