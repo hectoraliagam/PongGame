a70dbabf90fdb6714a9d06b1cb1e691bc1b8a23d
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-27 09:58:39 -0500

    redo ball script

--- src/ball.cpp
+++ src/ball.cpp
@@ -1,6 +1,6 @@
-#include "ball.h"
 #include "raylib.h"
 #include "config.h"
+#include "ball.h"
 
 Ball::Ball()
     : x(Config::ballCenterX),
@@ -11,10 +11,24 @@ Ball::Ball()
 {
 }
 
+float Ball::GetCenterY() const
+{
+  return y + radius;
+}
+
 void Ball::Update()
 {
   x += speedX;
   y += speedY;
+
+  if (x + radius >= GetScreenWidth() || x - radius <= 0)
+  {
+    speedX *= -1;
+  }
+  if (y + radius >= GetScreenHeight() || y - radius <= 0)
+  {
+    speedY *= -1;
+  }
 }
 
 void Ball::Draw() const
