8bb0e3c257acc6713c6209a28cc2bc1fde3c3668
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2025-12-27 13:50:19 -0500

    add Colors

---
+++ include/Colors.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <raylib.h>
+
+namespace Colors
+{
+  inline constexpr Color Neutral{92, 120, 118, 255};
+  inline constexpr Color DarkBlue{48, 72, 86, 255};
+  inline constexpr Color LightBlue{138, 156, 168, 255};
+  inline constexpr Color Gray{170, 176, 182, 255};
+}
